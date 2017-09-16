6595ce95577a7b37630ca9ac833bd45b43418e80
Jonathan Guyer <guyer@nist.gov>
2017-09-16 09:20:36 -0400

    Fix typo

--- sumatra/recordstore/datreant_store.py
+++ sumatra/recordstore/datreant_store.py
@@ -64,7 +64,7 @@ class DatreantRecordStore(RecordStore):
         treant = dtr.Treant(treant=os.path.join(record.datastore.root,
                                                 record.label))
         path = treant[self.JSON_PATTERN.format(record.label)].make().abspath
-        with open(treant[path, 'w') as f:
+        with open(path, 'w') as f:
             f.write(serialization.encode_record(record))
         
         records.add(treant)
